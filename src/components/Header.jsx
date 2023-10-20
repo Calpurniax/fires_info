c7d74c6afc82ace1ed3c37a471b654a22ad4207e
Calpurniax <sofia.gracia@gmail.com>
2023-10-20 19:15:03 +0200

    git test

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Fires in Castilla y Leon information</h1>
+        </header>
+    )
+}; export default Header
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+
+const Landing = () => {
+  return (
+    <>
+      <Header />
+      <main>
+    
+      </main>
+    </>
+  );
+};
+export default Landing;
